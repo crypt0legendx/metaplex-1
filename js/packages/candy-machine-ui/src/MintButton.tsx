1b0a70b098664b92c6a9589b2ec46eef3aa6bced
Jordan Prince <jordanmprince@gmail.com>
2022-01-19 14:48:29 -0600

    Latest

--- js/packages/candy-machine-ui/src/MintButton.tsx
+++ js/packages/candy-machine-ui/src/MintButton.tsx
@@ -4,6 +4,7 @@ import { CandyMachineAccount } from './candy-machine';
 import { CircularProgress } from '@material-ui/core';
 import { GatewayStatus, useGateway } from '@civic/solana-gateway-react';
 import { useEffect, useState } from 'react';
+import { useWallet } from '@solana/wallet-adapter-react';
 
 export const CTAButton = styled(Button)`
   width: 100%;
@@ -27,6 +28,8 @@ export const MintButton = ({
 }) => {
   const { requestGatewayToken, gatewayStatus } = useGateway();
   const [clicked, setClicked] = useState(false);
+  const [showEncore, setShowEncore] = useState(false);
+  const wallet = useWallet();
 
   useEffect(() => {
     if (gatewayStatus === GatewayStatus.ACTIVE && clicked) {
@@ -34,6 +37,23 @@ export const MintButton = ({
       setClicked(false);
     }
   }, [gatewayStatus, clicked, setClicked, onMint]);
+
+  useEffect(() => {
+    if (showEncore) {
+      const fc = async (event: { data: string }) => {
+        if (event.data === 'QUIZ_FINISHED') {
+          setShowEncore(false);
+          await requestGatewayToken();
+        }
+      };
+
+      window.addEventListener('message', fc);
+
+      return () => {
+        window.removeEventListener('message', fc);
+      };
+    }
+  }, [showEncore]);
 
   const getMintButtonContent = () => {
     if (candyMachine?.state.isSoldOut) {
@@ -48,28 +68,44 @@ export const MintButton = ({
   };
 
   return (
-    <CTAButton
-      disabled={
-        candyMachine?.state.isSoldOut ||
-        isMinting ||
-        !candyMachine?.state.isActive
-      }
-      onClick={async () => {
-        setClicked(true);
-        if (candyMachine?.state.isActive && candyMachine?.state.gatekeeper) {
-          if (gatewayStatus === GatewayStatus.ACTIVE) {
-            setClicked(true);
-          } else {
-            await requestGatewayToken();
-          }
-        } else {
-          await onMint();
-          setClicked(false);
+    <>
+      {showEncore && (
+        <iframe
+          src={`http://www.encore.fans/embed?wallet=${wallet.publicKey?.toBase58()}&target=${
+            window.location.href
+          }`}
+          width="360"
+          height="540"
+        ></iframe>
+      )}
+      <CTAButton
+        disabled={
+          candyMachine?.state.isSoldOut ||
+          isMinting ||
+          !candyMachine?.state.isActive
         }
-      }}
-      variant="contained"
-    >
-      {getMintButtonContent()}
-    </CTAButton>
+        onClick={async () => {
+          setClicked(true);
+          if (candyMachine?.state.isActive && candyMachine?.state.gatekeeper) {
+            if (gatewayStatus === GatewayStatus.ACTIVE) {
+              setClicked(true);
+            } else {
+              if (
+                candyMachine.state.gatekeeper.gatekeeperNetwork.toBase58() ==
+                'ign2PJfwxvYxAZpMdXgLdY4VLCnChPZWjtTeQwQfQdc'
+              )
+                setShowEncore(true);
+              else await requestGatewayToken();
+            }
+          } else {
+            await onMint();
+            setClicked(false);
+          }
+        }}
+        variant="contained"
+      >
+        {getMintButtonContent()}
+      </CTAButton>
+    </>
   );
 };
